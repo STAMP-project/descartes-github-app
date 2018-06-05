6526580b52a5a8c51700f79f9219f2db6f29f174
Caroline Landry <caroline.landry@inria.fr>
2018-06-05 10:00:24 +0200

    adding script to clean temporary files and directories

---
+++ tools/clean_files.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+################################################################################
+
+rm information_*.json other_*.json pr_*.json  *.json.pretty
+rm -rf descartesWorkingDir
+
